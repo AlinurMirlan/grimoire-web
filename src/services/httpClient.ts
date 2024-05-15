94dfa89fc52ae77b86a0b5351862f3b5ac8f5590
Alinur Mirlan <a18nur.m1r1an@gmail.com>
2024-05-15 14:01:00 +0600

    chore: :construction_worker: change the http client's base url

--- src/services/httpClient.ts
+++ src/services/httpClient.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const httpClient = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "http://13.59.130.149",
   headers: {
     "Content-Type": "application/json",
   },
